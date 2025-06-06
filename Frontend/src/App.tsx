b5471599eb41c8f8ac98464b0d472ce30b65fe76
Vinesh Katewa <Vinesh.katewa@gmail.com>
2025-06-06 16:06:12 +0530

    Made initial files, configuration changes to fix typescript errors

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<div>Hello World</div>} />
+                    <Route path="/" element={<Dashboard />} />
                 </Routes>
             </BrowserRouter>
         </>
