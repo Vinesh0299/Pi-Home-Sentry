19beae59dba5ea0097c9bfbc0da47684fef03b2e
Vinesh Katewa <Vinesh.katewa@gmail.com>
2025-06-07 16:40:43 +0530

    Added .env file and added link to github to navbar

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -8,9 +8,9 @@ function Navbar() {
   return (
     <div className="flex min-h-screen w-full">
       <Sidebar />
-      <div className={`flex flex-row justify-start fixed pl-4 top-0 w-full bg-gray-900 transition-all duration-300 ${isExpanded ? 'ml-64 pr-68' : 'ml-16 pr-20'}`}>
+      <div className={`flex flex-row justify-end fixed pl-4 top-0 w-full bg-gray-900 transition-all duration-300 ${isExpanded ? 'ml-64 pr-68' : 'ml-16 pr-20'}`}>
         <div className="h-16 flex items-center">
-          <span className="text-white">Pi Home Sentry</span>
+          <a className="text-white hover:bg-gray-800 hover:cursor-pointer p-2 px-4 rounded-md" target="_blank" href="https://github.com/Vinesh0299/Pi-Home-Sentry/tree/main">Github</a>
         </div>
       </div>
     </div>
