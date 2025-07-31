0e6047b8bce3ee680afb1b1a5a22a8a3d0e11e67
Vinesh Katewa <Vinesh.katewa@gmail.com>
2025-07-31 21:23:56 +0530

    Added new repo for creating web server, added logger content that can be used from any file

--- Backend/cmd/server/main.go
+++ Backend/cmd/server/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/Vinesh0299/Pi-Home-Sentry/Backend/internal/utils"
+
 )
 
 func main() {
-	utils.InitializeLogger("./log.txt")
+	logger := utils.GetLogger()
+
+	logger.Debug("Used Debug logger in main function")
+	logger.Info("Used the logger in main function")
+	logger.Warn("Used the warn logger in main function")
+	logger.Error("Used the error logger in main function")
 }
--- Backend/internal/utils/logger.go
+++ Backend/internal/utils/logger.go
@@ -1,20 +1,46 @@
 package utils
 
 import (
-	"log"
+	"log/slog"
 	"os"
+	"io"
 )
 
 // Logger is a global logger instance.
-var Logger *log.Logger
+var Logger *slog.Logger
 
 // Function to initialize the logger
-func InitializeLogger(logPath string) {
-	file, err := os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+func initLogger(logPath string) {
+	file, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Fatalf("Failed to open log file: %v", err)
+		slog.Error("Failed to create log file", "Path:", logPath, "Error:", err)
+		os.Exit(1)
 	}
 
-	Logger = log.New(file, "", log.LstdFlags)
-	Logger.Println("Logger initialized")
+	// Create a mew writer for logs
+	writer := io.MultiWriter(file, os.Stdout)
+
+	// Create the new logger
+	Logger = slog.New(slog.NewJSONHandler(writer, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	Logger.Info("Logger initialized successfully")
+}
+
+// Function that will return the logger object
+func GetLogger() *slog.Logger {
+	// Create the logger if it does not exists
+	if Logger == nil {
+		logLocation := os.Getenv("LOG_LOCATION")
+
+		if logLocation == "" {
+			logLocation = "./log.txt"
+		}
+
+		initLogger(logLocation)
+	}
+
+	// Return the logger
+	return Logger
 }
