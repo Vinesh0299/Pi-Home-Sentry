f4ca336f897bebc2cfae5a1e0e38de359777353d
Vinesh Katewa <Vinesh.katewa@gmail.com>
2025-07-31 21:41:43 +0530

    Added TODOs

--- Backend/internal/server/router.go
+++ Backend/internal/server/router.go
@@ -8,11 +8,13 @@ import (
 func SetupRouter() *chi.Mux {
 	router := chi.NewRouter()
 
+	// TODO: Leading "/" should be ignored
 	router.Get("/ping", ping)
 
 	return router
 }
 
+// TODO: Convert to proper json return using types
 func ping(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusOK)
 	res.Write([]byte("pong"))
